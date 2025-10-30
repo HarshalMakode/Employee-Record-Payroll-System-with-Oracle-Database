04f7e84ce73993b7b45a44dd30792e6176b26472
HarshalMakode <harshalmakode26@gmail.com>
2025-10-31 00:27:26 +0530

    delete

--- backend/models/employeeModel.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE employees (
-  eid NUMBER(5),
-  ename VARCHAR2(100) NOT NULL,
-  designation VARCHAR2(100),
-  salary NUMBER(10,2),
-  CONSTRAINT employees_PK_eid PRIMARY KEY (eid),
-  CONSTRAINT employees_CK_salary CHECK (salary >= 0)
-);
